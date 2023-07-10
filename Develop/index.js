fb443046a6705b1504fd9e163c8cfb1d7a205ffb
GimmeKitties711 <131449161+GimmeKitties711@users.noreply.github.com>
2023-07-09 19:23:20 -0700

    filled questions array and required modules

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,9 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['Enter project title:', 'Enter a description:', 'Enter installation instructions:', 'Enter usage information:', 'Enter contribution guidelines:', 'Enter testing instructions', 'Choose a license:', 'Enter your GitHub username:', 'Enter your email address:'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
