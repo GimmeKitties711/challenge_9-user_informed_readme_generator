147e933086a5bbf7ad8970747946f6f231251977
GimmeKitties711 <131449161+GimmeKitties711@users.noreply.github.com>
2023-07-27 11:18:35 -0700

    testing program fully, installed search-list

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+inquirer.registerPrompt("search-list", require("inquirer-search-list"));
+// source for registering the search-list prompt: https://www.npmjs.com/package/inquirer-search-list
 const fs = require("fs");
-//const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-//const questions = ['Enter project title:', 'Enter a description:', 'Enter installation instructions:', 'Enter usage information:', 'Enter contribution guidelines:', 'Enter testing instructions', 'Choose a license:', 'Enter your GitHub username:', 'Enter your email address:'];
 
 // this array was written using the inquirer documentation: https://www.npmjs.com/package/inquirer
 const questions = [
@@ -16,6 +16,9 @@ const questions = [
         validate: function(answer) {
             if (answer.length < 1) {
                 return console.log("You must enter a project title.");
+            } else if (answer.search('\\t') !== -1) {
+                return console.log("You cannot include tab characters in your title.");
+                // source for the search method in strings: https://www.w3schools.com/jsref/jsref_search.asp
             }
             return true;
         }
@@ -34,7 +37,7 @@ const questions = [
     {
         type: "input",
         name: "installation",
-        message: "Enter instructions to install software needed to use your project, if applicable:",
+        message: "Enter instructions to install software needed to use your project, if applicable:"
     },
     {
         type: "input",
@@ -50,24 +53,24 @@ const questions = [
     {
         type: "input",
         name: "credits",
-        message: "List any collaborators or web resources that helped you complete your project, if applicable:",
+        message: "List any collaborators or web resources that helped you complete your project, if applicable:"
     },
     {
         type: "input",
         name: "contributing",
-        message: "Explain how users can contribute to your project, if applicable:",
+        message: "Explain how users can contribute to your project, if applicable:"
     },
     {
         type: "input",
         name: "tests",
-        message: "Explain how to test your project, if applicable:",
+        message: "Explain how to test your project, if applicable:"
     },
     {
-        type: "list",
+        type: "search-list",
         name: "license",
-        message: "Use the Enter key to select a license for your project:",
-        choices: ['Apache_2.0', 'GPL_v3', 'MIT', 'BSD_2--Clause', 'BSD_3--Clause', 'Boost_1.0', 'CC0_1.0', 'EPL_1.0', 'AGPL_v3', 'GPL_v2', 'LGPL_v2.1', 'MPL_2.0', 'Unlicense', 'none'],
-        // full names for the license choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense', 'none']. these are the choices that are available to someone creating a new repository in GitHub, a screenshot of this is shown in this project's readme.
+        message: "Select a license for your project by typing letters to search for its name:",
+        choices: ['Apache_2.0 (Apache License 2.0)', 'GPL_v3 (GNU General Public License v3.0)', 'MIT (MIT License)', 'BSD_2--Clause (BSD 2-Clause "Simplified" License)', 'BSD_3--Clause (BSD 3-Clause "New" or "Revised" License)', 'Boost_1.0 (Boost Software License 1.0)', 'CC0_1.0 (Creative Commons Zero v1.0 Universal)', 'EPL_1.0 (Eclipse Public License 2.0)', 'AGPL_v3 (GNU Affero General Public License v3.0)', 'GPL_v2 (GNU General Public License v2.0)', 'LGPL_v2.1 (GNU Lesser General Public License v2.1)', 'MPL_2.0 (Mozilla Public License 2.0)', 'Unlicense (The Unlicense)', 'none']
+        // these are the choices that are available when creating a new repository in GitHub. a screenshot of this is shown in this challenge's readme.
     },
     {
         type: "input",
@@ -100,19 +103,18 @@ function writeToFile(fileName, data) {
 
 function processTitleString(title) {
     // source for the replaceAll method in strings: https://www.w3schools.com/jsref/jsref_string_replaceall.asp
-    let processedString = title.replaceAll('\u0020', '\u005F');
-    // source for special character unicode: https://owasp.org/www-community/password-special-characters (this is the resource that was provided for challenge 3)
-    let forbiddenChars = ['\u005C', '\u002F', '\u003A', '\u002A', '\u003F', '\u0022', '\u003C', '\u003E', '\u007C'];
+    let processedString = title.replaceAll(' ', '_');
     processedString = processedString.replace(/[\\*\/*\:*\**\?*\"*\<*\>*\|*]/g, "-"); // source for how to use RegExp: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions
     // source for the replace method in strings: https://www.w3schools.com/jsref/jsref_replace.asp
-    processedString = `./test/${processedString}_README.md`;
+    processedString = `./example/${processedString}_README.md`;
     return processedString;
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    console.log("\x1B[1mImportant (1/2):\x1B[m Inquirer only allows you to write on one line. Pressing the Enter key will move you to the next question. If you want to write a new line, use the \x1B[1m<br>\x1B[m tag. For example: This is line 1.<br> This is line 2.");
-    console.log("\x1B[1mImportant (2/2):\x1B[m The writeFileSync function requires unique file names. This README generator writes file names as \x1B[1mtitle_README.md,\x1B[m so if you use the same title for two different README files, the first one will be overwritten.");
+    console.log("\x1B[1mImportant (1/3):\x1B[m Inquirer only allows you to write on one line. Pressing the Enter key will move you to the next question. If you want to write a new line, use the \x1B[1m<br>\x1B[m tag. For example: This is line 1.<br> This is line 2.");
+    console.log("\x1B[1mImportant (2/3):\x1B[m The writeFileSync function requires unique file names. This README generator writes file names as \x1B[1mtitle_README.md,\x1B[m so if you use the same title for two different README files, the first one will be overwritten.");
+    console.log("\x1B[1mImportant (3/3):\x1B[m Your title cannot include a tab because it causes problems with file creation. A tab is written as \x1B[1m\\t,\x1B[m and the backslash is not allowed in file names.");
     // source for console logging bolded text using ANSI (American National Standards Institute) escape sequences: https://developer.chrome.com/docs/devtools/console/format-style/#style-console-messages
     inquirer.prompt(questions).then((responses) => {
         console.log("Creating a professional README file for your project...");
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,10 +4,11 @@ let licenses = ['Apache_2.0', 'GPL_v3', 'MIT', 'BSD_2--Clause', 'BSD_3--Clause',
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   let license_colors = ['blue', 'blue', 'yellow', 'orange', 'orange', 'lightblue', 'lightgrey', 'red', 'blue', 'blue', 'blue', 'brightgreen', 'blue']; // source for the colors that correspond to the licenses: https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
-  let chosenLicense = licenses.indexOf(license); // source for finding the first index that corresponds to an element in an array: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+  let chosenLicense = licenses.indexOf(license.split(" ")[0]); // source for finding the first index that corresponds to an element in an array: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
   // this method can be used because every entry in the licenses array is unique
+  // source for the split method in strings: https://www.w3schools.com/jsref/jsref_split.asp
   if (license !== 'none') { // if a license has been selected
-    return `![GitHub license](https://img.shields.io/badge/License-${license}-${license_colors[chosenLicense]}.svg)`;
+    return `![GitHub license](https://img.shields.io/badge/License-${license.split(" ")[0]}-${license_colors[chosenLicense]}.svg)`;
   }
   return ""; // if no license has been selected
 }
@@ -25,12 +26,12 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   let licenseFullNames = ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal license', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'];
-  let licenseLinks = ['https://www.apache.org/licenses/LICENSE-2.0', 'https://www.gnu.org/licenses/gpl-3.0.en.html', 'https://opensource.org/license/mit/', 'https://opensource.org/license/bsd-2-clause/', 'https://opensource.org/license/bsd-3-clause/', 'https://www.boost.org/LICENSE_1_0.txt', 'https://creativecommons.org/publicdomain/zero/1.0/legalcode', 'https://www.eclipse.org/legal/epl-2.0/', 'https://www.gnu.org/licenses/agpl-3.0.en.html', 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html', 'https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html', 'https://www.mozilla.org/en-US/MPL/2.0/', 'https://choosealicense.com/licenses/unlicense/'];
-  let chosenLicense = licenses.indexOf(license);
-  if (license !== 'none' && license !== 'Unlicense') {
+  let licenseLinks = ['https://www.apache.org/licenses/LICENSE-2.0', 'https://www.gnu.org/licenses/gpl-3.0.en.html', 'https://opensource.org/license/mit/', 'https://opensource.org/license/bsd-2-clause/', 'https://opensource.org/license/bsd-3-clause/', 'https://www.boost.org/users/license.html', 'https://creativecommons.org/publicdomain/zero/1.0/legalcode', 'https://www.eclipse.org/legal/epl-2.0/', 'https://www.gnu.org/licenses/agpl-3.0.en.html', 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html', 'https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html', 'https://www.mozilla.org/en-US/MPL/2.0/', 'https://choosealicense.com/licenses/unlicense/'];
+  let chosenLicense = licenses.indexOf(license.split(" ")[0]);
+  if (license !== 'none' && license.split(" ")[0] !== 'Unlicense') {
     return `## License
 This project was created using the ${licenseFullNames[chosenLicense]}. More information about the license can be found [here](${licenseLinks[chosenLicense]}).`;
-  } else if (license === 'Unlicense') {
+  } else if (license.split(" ")[0] === 'Unlicense') {
     return `## License
 This project was created using ${licenseFullNames[chosenLicense]}. More information about the license can be found [here](${licenseLinks[chosenLicense]}).`;
   }
