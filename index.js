543a7d61e654f181179115e58de2240487841f2e
GimmeKitties711 <131449161+GimmeKitties711@users.noreply.github.com>
2023-07-29 07:18:37 -0700

    edited comments

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const questions = [ // array of objects
                 return console.log("You cannot include tab characters in your title.");
                 // source for the search method in strings: https://www.w3schools.com/jsref/jsref_search.asp
             }
-            return true; // if a title without tab characters is given
+            return true; // if a valid title without tab characters is given
         }
     },
     {
@@ -70,7 +70,7 @@ const questions = [ // array of objects
         name: "license",
         message: "Select a license for your project by typing letters to search for its name:",
         choices: ['Apache_2.0 (Apache License 2.0)', 'GPL_v3 (GNU General Public License v3.0)', 'MIT (MIT License)', 'BSD_2--Clause (BSD 2-Clause "Simplified" License)', 'BSD_3--Clause (BSD 3-Clause "New" or "Revised" License)', 'Boost_1.0 (Boost Software License 1.0)', 'CC0_1.0 (Creative Commons Zero v1.0 Universal license)', 'EPL_1.0 (Eclipse Public License 2.0)', 'AGPL_v3 (GNU Affero General Public License v3.0)', 'GPL_v2 (GNU General Public License v2.0)', 'LGPL_v2.1 (GNU Lesser General Public License v2.1)', 'MPL_2.0 (Mozilla Public License 2.0)', 'Unlicense (The Unlicense)', 'none']
-        // these are the choices that are available when creating a new repository in GitHub. a screenshot of these options is shown in this challenge's readme.
+        // these are the choices that are available when creating a new repository in GitHub. a screenshot of these options is shown in this challenge's README.
     },
     {
         type: "input",
@@ -98,17 +98,17 @@ const questions = [ // array of objects
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(fileName, data); // source for the writeFileSync documentation: https://nodejs.org/api/fs.html#fswritefilesyncfile-data-options
+    return fs.writeFileSync(fileName, data); // writeFileSync documentation: https://nodejs.org/api/fs.html#fswritefilesyncfile-data-options
 }
 
 function processTitleString(title) {
     /*
-    the purpose of this function is to filter out characters that are not allowed in filenames. as shown in this challenge's README:
+    the purpose of this function is to filter out characters that are not allowed in file names. as shown in this challenge's README:
 
     A file name can't contain any of the following characters:
     \ / : * ? " < > |
 
-    the title is included in the file name, so if the title contains any of the forbidden characters, they need to be filtered out. I have decided to change all of them to the dash (-).
+    the title is included in the file name, so if the title contains any of the forbidden characters, they need to be filtered out. I have decided to change all of them to a dash (-).
     */
     // source for the replaceAll method in strings: https://www.w3schools.com/jsref/jsref_string_replaceall.asp
     let processedString = title.replaceAll(' ', '_');
@@ -124,7 +124,7 @@ function processTitleString(title) {
 
 // TODO: Create a function to initialize app
 function init() {
-    // important precautions that the user should take when answering the prompts to write a new README file
+    // important precautions that the user should take when answering the prompts for a new README file
     console.log("\x1B[1mImportant (1/3):\x1B[m Inquirer only allows you to write on one line. Pressing the Enter key will move you to the next question. If you want to write a new line, use the \x1B[1m<br>\x1B[m tag. For example: This is line 1.<br> This is line 2.");
     console.log("\x1B[1mImportant (2/3):\x1B[m The writeFileSync function requires unique file names. This README generator writes file names as \x1B[1mtitle_README.md,\x1B[m so if you use the same title for two different README files, the first one will be overwritten.");
     console.log("\x1B[1mImportant (3/3):\x1B[m Your title cannot include a tab because it causes problems with file creation. A tab is written as \x1B[1m\\t,\x1B[m and the backslash is not allowed in file names.");
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -35,15 +35,15 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   let licenseLinks = ['https://www.apache.org/licenses/LICENSE-2.0', 'https://www.gnu.org/licenses/gpl-3.0.en.html', 'https://opensource.org/license/mit/', 'https://opensource.org/license/bsd-2-clause/', 'https://opensource.org/license/bsd-3-clause/', 'https://www.boost.org/users/license.html', 'https://creativecommons.org/publicdomain/zero/1.0/legalcode', 'https://www.eclipse.org/legal/epl-2.0/', 'https://www.gnu.org/licenses/agpl-3.0.en.html', 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html', 'https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html', 'https://www.mozilla.org/en-US/MPL/2.0/', 'https://choosealicense.com/licenses/unlicense/'];
-  let chosenLicense = licenses.indexOf(license.split(" ")[0]); // same as in renderLicenseBadge()
+  let chosenLicense = licenses.indexOf(license.split(" ")[0]);
   if (license !== 'none' && license.split(" ")[0] !== 'Unlicense') {
     return `## License
 This project was created using the ${license.split("(")[1].slice(0,-1)}. More information about the license can be found [here](${licenseLinks[chosenLicense]}).`; // the first insert is the full name of the license, the second insert is a link to more information about the license
-// as mentioned in the comments for renderLicenseBadge, the licenses are formatted as 'code (full name)'. license.split("(") will return the array [code , full name)]. the second entry can be obtained with index 1, and the unwanted parenthesis is removed using the slice method, which returns the string up to but not including the last character. '-1' means the same thing as 'length-1', which means that the last character is omitted. source for the slice method in strings: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+// as mentioned in the comments for renderLicenseBadge(), the licenses are formatted as 'code (full name)'. license.split("(") will return the array [code , full name)]. the second entry can be obtained with index 1, and the unwanted parenthesis is removed using the slice method, which returns the string starting from [first parameter] up to but not including [second parameter]. '-1' means the same thing as 'length-1', which means that the last character is omitted. source for the slice method in strings: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
   } else if (license.split(" ")[0] === 'Unlicense') {
     return `## License
 This project was created using ${license.split("(")[1].slice(0,-1)}. More information about the license can be found [here](${licenseLinks[chosenLicense]}).`;
-// the name 'The Unlicense' is the only full license name that starts with the word 'The', so the purpose of this block of code is to avoid saying 'This project was created using the The Unlicense.'
+// the name 'The Unlicense' is the only license name that starts with the word 'The', so the purpose of this block of code is to avoid saying 'This project was created using the The Unlicense.'
   }
   return "";
 }
